03e3abe1c242a36419873a6646bb04b261390382
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2021-03-09 20:58:25 -0300

    add repositorios de Aluno

---
+++ Projeto/src/Repository/RepositorioAlunos.java
@@ -0,0 +1,13 @@
+package Repository;
+
+import models.Aluno;
+
+public interface RepositorioAlunos {
+
+    public void inserir(Aluno aluno);
+
+    public Aluno procurar(String cpfAluno);
+
+    public void remover(String cpfAluno);
+
+}
